bcf7628d39cdf504ae88dbc4185bed7fd378b5f7
Shubham Gupta <gshubham55@gmail.com>
2013-06-23 16:48:48 +0530

    Addition: Old and new basic navbar

---
+++ old/inc/header.php
@@ -0,0 +1,17 @@
+<?php
+	echo "
+	<div class='blueTop'>
+		<nav>
+			<ul>
+				<li><a href='#' class='linkNavElements'><span class='spanNavElements'>(SDSLabs Logo)</span></a></li>
+				<li><a href='#' class='linkNavElements'><span class='spanNavElements'>Home &nbsp <span class='lol'>&#10072</span></span></a></li>
+				<li><a href='#' class='linkNavElements'><span class='spanNavElements'>About &nbsp &#10072</span></a></li>
+				<li><a href='#' class='linkNavElements'><span class='spanNavElements'>Work &nbsp &#10072</span></a></li>
+				<li><a href='#' class='linkNavElements'><span class='spanNavElements'>Contact Us &nbsp &#10072</span></a></li>
+				<li><a href='#' class='linkNavElements'><span class='spanNavElements'>Members &nbsp &#10072</span></a></li>
+				<li><a href='#' class='linkNavElements'><span class='spanNavElements'>Blog</span></a></li>
+				<li><input type='text'></li>	
+			</ul>
+		</nav>
+	</div>"
+?>
---
+++ old/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>SDSLabs</title>
+		<link rel="stylesheet" href="css/custom.css">
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+		<div id="blueTop"></div>
+		<div id="whiteLine"></div>
+		<div id="nav">
+				<nav>
+					<ul>
+						<li><a href='#' ><span class="textSpan">(SDSLabs Logo)</span></a><span class='divider'>&nbsp&nbsp&nbsp&nbsp</span></li>
+						<li><a href='#' ><span class="textSpan" id="currentPage">&nbsp Home &nbsp</span></a>  <span class='divider'>&#10072</span></li>
+						<li><a href='#' ><span class="textSpan">About</span></a>  <span class='divider'>&#10072</span></span></li>
+						<li><a href='#' ><span class="textSpan">Work</span></a>  <span class='divider'>&#10072</span></span></li>
+						<li><a href='#' ><span class="textSpan">Contact Us</span></a>  <span class='divider'>&#10072</span></span></li>
+						<li><a href='#' ><span class="textSpan">Members</span></a>  <span class='divider'>&#10072</span></span></li>
+						<li><a href='#' ><span class="textSpan">Blog</span></a><span class='divider'>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</span></li>
+						<li id="searchBox"><form id="search"><input type='text' name="search" autocomplete="off" id="search" placeholder="Search.."></form><span class='divider'></span></li>	
+					</ul>
+				</nav>
+		</div>
+		<div class="hero-unit" id="titleDivHome">
+			<div id="leftTitleHome">
+				<img src="images/idivii2.png" id="iDiCiI"><!--  <br><br><br><br><br><br><br><br><br><br><br> -->
+				<!-- <img src="images/dash.jpg"> -->
+				<p class="dashTitle">_____________________________________________________________</p> <!-- Get the actual image for the line -->
+				<p class="titleText">SDSLabs (Software Development Section) is a student section at IIT Roorkee that promises various technical activities. We promote technical activities in campus that raise the technical know-how and help the average student in his daily activities.</p> 
+			</div>
+			<div id="rightTitleHome">
+				<img src="images/mascot.png" id="mascot1">
+			</div>
+		</div>	
+	</body>
+</html>
